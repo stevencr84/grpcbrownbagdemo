8878204c7a9a80c119030705e104650ae0376709
Steven Carr-Rollitt <steven.carr-rollit@imbursepayments.com>
2021-08-27 12:46:08 +0200

    initial commit

---
+++ src/GrpcClient/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Net.Client;
+using GrpcServer;
+
+namespace GrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await SayHelloStreaming();
+        }
+
+        private static async Task SayHelloUnary()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = await client.SayHelloAsync(new HelloRequest
+            {
+                Name = "Brownbag demo"
+            });
+
+            Console.WriteLine(response.Message);
+        }
+
+        private static async Task SayHelloStreaming()
+        {
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+
+            var response = client.SayHelloStream(new HelloRequest
+            {
+                Name = "Brownbag demo straming"
+            });
+
+            await foreach(var item in response.ResponseStream.ReadAllAsync())
+            {
+                Console.WriteLine($"Response from server: {item.Message}");
+            }
+        }
+    }
+}
---
+++ src/GrpcServer/Services/GreeterService.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace GrpcServer
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+
+        public override async Task SayHelloStream(HelloRequest request, IServerStreamWriter<HelloReply> responseStream, ServerCallContext context)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                await responseStream.WriteAsync(new HelloReply()
+                {
+                    Message = $"Hello {request.Name} {i}"
+                });
+
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+        }
+    }
+}
